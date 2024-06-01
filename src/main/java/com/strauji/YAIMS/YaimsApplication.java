ceaf5db902bc192cf255d1795b22d0808e2ae0d3
Strauji <Strauji@users.noreply.github.com>
2024-06-01 01:19:15 -0300

    Initial Commit

---
+++ src/main/java/com/strauji/YAIMS/YaimsApplication.java
@@ -0,0 +1,13 @@
+package com.strauji.YAIMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaimsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strauji/YAIMS/YaimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strauji.YAIMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
